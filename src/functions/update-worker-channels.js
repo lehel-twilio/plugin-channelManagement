7a554db4eb53e79d26b3f3ffdd87380d8bf1e0bd
Adam Watson <adamwatson@adams-mbp-2.lan>
2019-05-28 19:32:59 -0700

    Use array payload and promise.all

--- src/functions/update-worker-channels.js
+++ src/functions/update-worker-channels.js
@@ -1,71 +1,50 @@
 exports.handler = function(context, event, callback) {
-
-	const response = new Twilio.Response();
-    response.appendHeader('Access-Control-Allow-Origin', '*');
-    response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
-    response.appendHeader('Content-Type', 'application/json');
-    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-	let client = context.getTwilioClient();
-
-	console.log(event.WorkerSid);
-
-	let channels = [];
-	const channelCapacity = JSON.parse(event.channelCapacity);
-	const channelAvailability = JSON.parse(event.channelAvailability);
-	channels = Object.keys(channelCapacity);
-
-	for (let i = 0; i < channels.length; i++) {
-	    let channel = channels[i];
-	    let j = i;
-
-	    console.log(i);
-	    console.log(j);
-	    console.log(event.WorkerSid);
-	    console.log(channel);
-
-	    client
-            .taskrouter
-            .workspaces(context.TWILIO_WORKSPACE_SID)
-            .workers(event.WorkerSid)
-            .workerChannels(channel)
-            .update({
-              capacity: channelCapacity[channel],
-              available: channelAvailability[channel]
-            })
-            .then(result => {
-                console.log('success');
-
-                console.log(i);
-                console.log(j);
-                console.log(channels.length -1);
-
-
-                if (j === (channels.length -1)) {
-
-                    console.log(j);
-
-                    client
-                        .taskrouter
-                        .workspaces(context.TWILIO_WORKSPACE_SID)
-                        .workers(event.WorkerSid)
-                        .workerChannels
-                        .list()
-                        .then(result => {
-                            console.log(result);
-
-                            response.body = result;
-                            callback(null, response);
-                          })
-                        .catch(error => {
-                            console.log(error);
-                            callback(error);
-                        });
-                }
-            })
-            .catch(error => {
-                console.log(error);
-                callback(error);
-            });
-	}
-};
+    const response = new Twilio.Response();
+    response.appendHeader("Access-Control-Allow-Origin", "*");
+    response.appendHeader("Access-Control-Allow-Methods", "OPTIONS POST");
+    response.appendHeader("Content-Type", "application/json");
+    response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+  
+    const client = context.getTwilioClient();
+    const channels = JSON.parse(event.channels);
+    const WorkspaceSid = context.TWILIO_WORKSPACE_SID;
+    const WorkerSid = event.WorkerSid
+  
+    const promises = channels.map(channel => {
+      return new Promise((resolve, reject) => {
+        client.taskrouter
+          .workspaces(WorkspaceSid)
+          .workers(WorkerSid)
+          .workerChannels(channel.name)
+          .update({
+            capacity: channel.capacity,
+            available: channel.isAvailable
+          })
+          .then(result => {
+            console.log(result, "result");
+            resolve(result);
+          })
+          .catch(error => {
+            console.log(error, "error");
+            reject(error);
+          });
+      });
+    });
+  
+    Promise.all(promises).then( data => {
+      client.taskrouter
+        .workspaces(WorkspaceSid)
+        .workers(WorkerSid)
+        .workerChannels.list()
+        .then(result => {
+          console.log(result);
+          response.body = result;
+          callback(null, response);
+        })
+        .catch(error => {
+          console.log(error);
+          callback(error);
+        });
+    });
+  };
+  
